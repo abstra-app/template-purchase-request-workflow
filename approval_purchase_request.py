591fbf34a9a5b27017d8f1a2ddeddf3513d0bbcc
Seibel <jpseibelcervo@yahoo.com.br>
2025-03-26 20:03:50 -0300

    approval purchase working

--- approval_purchase_request.py
+++ approval_purchase_request.py
@@ -1,17 +1,5 @@
-from abstra.compat import use_legacy_threads
-"""
-Calling the use_legacy_threads function allows using
-the legacy threads in versions > 3.0.0
-https://abstra.io/docs/guides/use-legacy-threads/
-
-The new way of using workflows is with tasks. Learn more
-at https://abstra.io/docs/concepts/tasks/ and contact us
-on any issues during your migration
-"""
-use_legacy_threads("forms")
-
 from abstra.forms import *
-from abstra.workflows import *
+from abstra.tasks import *
 from abstra.tables import *
 
 
@@ -23,26 +11,26 @@ def team_info_by_email(email):
 
 
 # verify if the requester is a valid user
-assignee_emails = get_data("assignee_emails")
-data = get_data(
-    "purchase_data")
-description = data["description"]
-amount = data["amount"]
-quantity = data["quantity"]
-reason = data["reason"]
-deadline = data["deadline"]
-requester_team_name = data["requester_team_name"]
-requester_team_id = data["requester_team_id"]
-requester_team_position = data["requester_team_position"]
-purchase_request_id = data["purchase_request_id"]
+tasks = get_tasks()
+task = tasks[0]
+payload = task.get_payload()
+
+assignee_emails = payload["assignee_emails"]
+purchase_request_status = payload["purchase_request_status"]
+purchase_data = payload["purchase_data"]
+
+description = purchase_data["description"]
+amount = purchase_data["amount"]
+quantity = purchase_data["quantity"]
+reason = purchase_data["reason"]
+deadline = purchase_data["deadline"]
+requester_team_name = purchase_data["requester_team_name"]
+requester_team_id = purchase_data["requester_team_id"]
+requester_team_position = purchase_data["requester_team_position"]
+purchase_request_id = purchase_data["purchase_request_id"]
 
 
-purchase_request_status = get_data("purchase_request_status")
-
-assignee_emails = get_data("assignee_emails")
-approval_team_email = get_data("item")
-if approval_team_email == None:
-    approval_team_email = assignee_emails[0]
+approval_team_email = assignee_emails[0]
 
 # builds the purchase approval page
 approval_page = Page().display("Purchase Request Approval", size="large")\
@@ -73,10 +61,17 @@ else:
     rejection_page = Page().display("Purchase Request Rejection", size="large")\
                            .read_textarea("Reason for Rejection", key="rejection_reason")\
                            .run("Send")
-    set_data("rejection_reason", rejection_page["rejection_reason"])
+    payload.update({"rejection_reason": rejection_page["rejection_reason"]})
 
 
 approval_team_id = team_info_by_email(approval_team_email)["id"]
 
 update("purchase_request_approvals", set={"approved": purchase_approved}, 
        where={"purchase_request_id": purchase_request_id, "team_id": approval_team_id})
+
+if len(assignee_emails) > 1:
+    update("purchase_request_approvals", set={"approved": purchase_approved}, 
+       where={"purchase_request_id": purchase_request_id})
+
+send_task("approval_status", payload)
+task.complete()
